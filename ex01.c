778007974687e0610bfc771dda6f871cc8b8fb40
gileduardo <gilgea@gmail.com>
2022-06-07 20:59:36 -0300

    Primeiro

---
+++ ex01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int num;
+
+    printf("Digite um número: ");
+    scanf("%i", &num);
+    
+    printf("Número digitado: %i", num);
+    
+    printf("\n");
+    return 0;
+}
---
+++ ex02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    char letra;
+
+    printf("Digite uma letra: ");
+    scanf("%c", &letra);
+    
+    printf("Letra digitada: %c", letra);
+    
+    printf("\n");
+    return 0;
+}
