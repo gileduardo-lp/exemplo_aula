b802f7ff9835f17f648d94399230a5f8c8bf260f
gileduardo <gilgea@gmail.com>
2022-06-08 18:21:56 -0300

    novo arquivo apos clonar

---
+++ ex03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    float f;
+
+    printf("Digite um ponto flutuante: ");
+    scanf("%f", &f);
+    
+    printf("Ponto flutuante: %f", f);
+    
+    printf("\n");
+    return 0;
+}
